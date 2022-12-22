970951785a210c5f6f74f66bdab2c878b5577685
Yuriy Pashkov <yuriy.pashkov@gmail.com>
2022-12-22 18:12:22 +0530

    Version 0.0.1, build 1
    Create project

---
+++ LetsMeet/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LetsMeet
+//
+//  Created by Yuriy Pashkov on 22.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
